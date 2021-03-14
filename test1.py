7c4a98f8e482c7efdb4b20251c41f609bad65290
Tejas-Shanbhag <tejasshanbhag14@gmail.com>
2021-03-14 13:22:59 +0530

    'enter'

---
+++ test1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd 
+import sklearn
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+print('done')
+plt.plot([1,2,3],[4,5,6])
+plt.grid()
+plt.show()
